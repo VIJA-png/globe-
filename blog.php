0a1724a11c27a109cd99708e08dde31e59ab4465
Vijay Kumar <67546917+VIJA-png@users.noreply.github.com>
2022-01-31 12:45:40 +0530

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Globe-Trotting Management system</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style>
+    body {
+      background-color: gray;
+  }
+
+  .footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: white;
+  color: black;
+  text-align: center;
+}
+  </style>
+</head>
+
+<body>
+<div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-md">
+    <a class="navbar-brand" href="index.php">Globe-Trotting Management System - Blog photos </a>
+    <div>
+      <a class="navbar-brand" href="index.php">click here - TO BOOK A TRIP</a>
+</div>
+  </div>
+</nav>
+<div>
+<div class="container-md">
+  <img src="3.jpg" alt="Trulli" width="500" height="333">
+  <img src="123.jpeg" alt="Trulli" width="500" height="333">
+  <img src="3.jpg" alt="Trulli" width="500" height="333">
+  <img src="1234.jpeg" alt="Trulli" width="500" height="333">
+  <img src="3.jpg" alt="Trulli" width="500" height="333">
+  <img src="12345.jpeg" alt="Trulli" width="500" height="333">
+</div>
+</div>
+
+
+<div>
+<div class="footer">
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2022 GMS . All Rights Reserved </p>
+	</div>
+</div>
+</div>
+</body>
+</html>
